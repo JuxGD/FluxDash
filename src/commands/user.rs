a1e3958d3ca9f85965138dfe283abaa9be114a2d
JuxGD <jak@e.email>
2026-04-04 10:51:06 -0300

    typo

--- src/commands/user.rs
+++ src/commands/user.rs
@@ -21,13 +21,13 @@ async fn execute(fluxer_api: &FluxerApiHandler, feedback: &CommandFeedback) {
     let message =
         "Information for user: **".to_owned() + &user.username + "**\n"
     +   "Stars: " + &user.stars.to_string() + "\n"
-    +   "Moons: " &user.moons.to_string() + "\n"
-    +   "Diamonds: " &user.diamonds.to_string() + "\n"
+    +   "Moons: " + &user.moons.to_string() + "\n"
+    +   "Diamonds: " + &user.diamonds.to_string() + "\n"
     +   "User Coins: " + &user.usercoins.to_string() + "\n"
-    +   "Secret Coins: " &user.secretcoins.to_string() + "\n"
-    +   "Demons: " &user.demons.to_string() + "\n"
-    +   "Leaderboard Spot: " &user.top.to_string() + "\n"
-    +   "Creator Points: " &user.ctpoints.to_string()
+    +   "Secret Coins: " + &user.secretcoins.to_string() + "\n"
+    +   "Demons: " + &user.demons.to_string() + "\n"
+    +   "Leaderboard Spot: " + &user.top.to_string() + "\n"
+    +   "Creator Points: " + &user.ctpoints.to_string()
     ;
 
 
